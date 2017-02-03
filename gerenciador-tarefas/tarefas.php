e210965af6a09dffd91ac187ec919bae17dd325a
Jonilson Sousa <jonilsondeveloper@gmail.com>
2017-02-03 13:57:55 -0300

    tarefas concluidas

--- gerenciador-tarefas/tarefas.php
+++ gerenciador-tarefas/tarefas.php
@@ -1,3 +1,9 @@
+<?php
+
+  session_start();
+
+ ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,13 +13,39 @@
   <body>
     <h1>Gerenciador de Tarefas</h1><br/>
 
-    <form class="" action="index.html" method="post">
+    <form class="" action="" method="get">
         <legend>Nova Tarefa</legend><br/>
         <label for="nome">Tarefa: </label>
         <input type="text" name="nome"/>
         <br/><br/><br/>
         <input type="submit" name="cadastrar" value="Cadastrar">
     </form>
+
+    <?php
+      if (isset($_GET['nome'])) {
+        $_SESSION['lista_tarefas'][] = $_GET['nome'];
+      }
+
+      $lista_tarefas = array();
+
+      if (isset($_SESSION['lista_tarefas'])) {
+        $lista_tarefas = $_SESSION['lista_tarefas'];
+      }
+     ?>
+
+     <table>
+       <tr>
+         <th>
+           Tarefas
+         </th>
+       </tr>
+
+       <?php   foreach($lista_tarefas as $item):  ?>
+       <tr>
+         <td>   <?php echo $item; ?>   </td>
+       </tr>
+     <?php endforeach  ?>
+     </table>
 
   </body>
 </html>
